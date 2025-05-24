5495741e7d82d62ef2a003d35230667c054412b5
Afnan-byte <afnan.messaging@gmail.com>
2025-05-24 15:39:23 +0530

    Initial commit

--- api/models.py
+++ api/models.py
@@ -5,7 +5,7 @@ class Registrant(models.Model):
     last_name = models.CharField(max_length=100)
     email = models.EmailField(unique=True)
     country_code = models.CharField(max_length=10)
-    mobile = models.CharField(max_length=20)
+    mobile = models.CharField(max_length=20,unique=True)
     company = models.CharField(max_length=200)
 
     def __str__(self):
