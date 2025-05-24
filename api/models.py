d47ddbe74c7d68fde181f2d586aa29ec7a206a32
Afnan-byte <afnan.messaging@gmail.com>
2025-05-24 22:27:31 +0530

    Initial commit

--- api/models.py
+++ api/models.py
@@ -1,12 +1,20 @@
 from django.db import models
+from django.core.validators import RegexValidator
 
 class Registrant(models.Model):
     first_name = models.CharField(max_length=100)
     last_name = models.CharField(max_length=100)
     email = models.EmailField(unique=True)
     country_code = models.CharField(max_length=10)
-    mobile = models.CharField(max_length=20,unique=True)
+
+    mobile_validator = RegexValidator(
+        regex=r'^\d{10}$',
+        message="Mobile number must be exactly 10 digits."
+    )
+    mobile = models.CharField(max_length=10, unique=True, validators=[mobile_validator])
+
     company = models.CharField(max_length=200)
+    profession = models.CharField(max_length=200)
 
     def __str__(self):
         return f"{self.first_name} {self.last_name}"
