e890b4277f32f02670683c14189351317a196e7d
Afnan-byte <afnan.messaging@gmail.com>
2025-05-24 06:10:41 +0530

    Initial commit

--- scaleupbackend/settings.py
+++ scaleupbackend/settings.py
@@ -93,5 +93,6 @@ CORS_ALLOW_ALL_ORIGINS = True
 CORS_ALLOWED_ORIGINS = [
     'https://dubai.scaleupconclave.com',
 ]
-ALLOWED_HOSTS = ['scaleup_backend.onrender.com', 'dubai.scaleupconclave.com']
+# ALLOWED_HOSTS = ['scaleup_backend.onrender.com', 'dubai.scaleupconclave.com']
+ALLOWED_HOSTS =['*']
 
