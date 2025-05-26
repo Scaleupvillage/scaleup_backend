fd90f3ea791592f1f00e6bf51709f4f2a814e4bf
Afnan-byte <afnan.messaging@gmail.com>
2025-05-26 21:30:58 +0530

    first commit

--- scaleup_backend/settings.py
+++ scaleup_backend/settings.py
@@ -26,8 +26,11 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # SECRET_KEY = 'django-insecure-qw5-v&9p7@%ft2e_$=#iq$e&eu(1%iwb+^b$j#2n$&t5c87i4z'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
-# SECRET_KEY = os.environ.get('SECRET_KEY')
+# DEBUG = True
+
+DEBUG = os.environ.get('DEBUG', 'False') == 'True'
+ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS', '').split(',')
+SECRET_KEY = os.environ.get('SECRET_KEY')
 # ALLOWED_HOSTS = ['your-backend.onrender.com', 'dubai.scaleupconclave.com']
 
 
@@ -74,13 +77,15 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'scaleup_backend.wsgi.application'
 
-DATABASES = {
-    "default":{
-        "ENGINE":"django.db.backends.sqlite3",
-        "NAME" : BASE_DIR / "db.sqlite3"
-    }
-}
-DATABASES["default"] = dj_database_url.parse("postgresql://scaleup_db_8l3c_user:qYuudaCEz3T7zbuFG1FDJrYLFbd4H7lB@dpg-d0q3ummmcj7s73ene4lg-a.oregon-postgres.render.com/scaleup_db_8l3c")
+# DATABASES = {
+#     "default":{
+#         "ENGINE":"django.db.backends.sqlite3",
+#         "NAME" : BASE_DIR / "db.sqlite3"
+#     }
+# }
+database_url = os.getenv("DATABASE_URL")
+
+DATABASES["default"] = dj_database_url.parse("database_url")
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
