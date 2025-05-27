9578e544987f16e54919282c84771192c090c873
Afnan-byte <afnan.messaging@gmail.com>
2025-05-27 06:22:06 +0530

    add req

--- scaleup_backend/settings.py
+++ scaleup_backend/settings.py
@@ -145,3 +145,12 @@ STATIC_URL = '/static/'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+# settings.py
+INSTALLED_APPS += ['corsheaders']
+MIDDLEWARE = ['corsheaders.middleware.CorsMiddleware'] + MIDDLEWARE
+
+CORS_ALLOW_ALL_ORIGINS = False
+CORS_ALLOWED_ORIGINS = [
+    "https://dubai.scaleupconclave.com",
+    "http://localhost:3000",  # For dev
+]
