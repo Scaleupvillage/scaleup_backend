7e3ba68237a75ba7b1af4b7e13c5146fc701d41a
Afnan-byte <afnan.messaging@gmail.com>
2025-05-28 14:20:04 +0530

    add req

--- scaleup_backend/settings.py
+++ scaleup_backend/settings.py
@@ -162,10 +162,12 @@ CORS_ALLOWED_ORIGINS = [
 # logger.setLevel(logging.DEBUG)
 # logger.addHandler(logging.StreamHandler())
 
+
+
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_HOST = 'email-smtp.us-east-1.amazonaws.com'  # Use your region
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER')  # Use Render environment variable
-EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASSWORD')
-DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
+EMAIL_HOST_USER = os.environ.get('SES_SMTP_USERNAME')
+EMAIL_HOST_PASSWORD = os.environ.get('SES_SMTP_PASSWORD')
+DEFAULT_FROM_EMAIL = 'scaleupvillage@gmail.com'
